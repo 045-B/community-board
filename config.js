cd57fe7d1543e498c5586349a9cd15f4e5323dec
045 <owou78@gmail.com>
2026-08-31 01:57:10 +0900

    Rename site and community categories

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.BOARD_CONFIG = {
   supabaseUrl: 'https://ooqryddarkazfcbesito.supabase.co',
   supabaseAnonKey: 'sb_publishable_qvcKdyx1RqI71ezmenZVGw_OXpy6CjA',
-  siteName: '우리들의 아카이브'
+  siteName: '앵탐라'
 };
 
