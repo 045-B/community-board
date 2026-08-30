9af3a331dfb22d45fa6d1d0674378e835c52f6b5
045 <owou78@gmail.com>
2026-08-31 01:49:25 +0900

    Connect Supabase backend

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.BOARD_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://ooqryddarkazfcbesito.supabase.co',
+  supabaseAnonKey: 'sb_publishable_qvcKdyx1RqI71ezmenZVGw_OXpy6CjA',
   siteName: '우리들의 아카이브'
 };
+
